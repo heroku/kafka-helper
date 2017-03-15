05fce0f5e4018ff528d61fdce1b07abe297a377a
Chris Lambacher <clambacher@heroku.com>
2017-03-15 13:58:49 -0400

    Support Python 3

--- setup.py
+++ setup.py
@@ -10,5 +10,16 @@ setup(
   url = 'https://github.com/heroku/kafka-helper',
   download_url = 'https://github.com/heroku/kafka-helper/tarball/0.1',
   keywords = ['kafka', 'heroku', 'kafka-python'],
-  classifiers = [],
+  install_requires=[
+      'cryptography',
+  ],
+  license='MIT',
+  classifiers = [
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Development Status :: 4 - Beta',
+      'Topic :: Text Processing :: Linguistic',
+  ],
+
 )
